7e6ce2818e3c9086273e1d5ded869ac0954cd355
TilekSydykov <tilekfromkg@gmail.com>
2019-04-25 19:12:29 +0600

    new module

--- app/src/main/java/com/flaterlab/dexbuilder/network/PostFlipdex.java
+++ app/src/main/java/com/flaterlab/dexbuilder/network/PostFlipdex.java
@@ -2,27 +2,38 @@ package com.flaterlab.dexbuilder.network;
 
 import java.io.IOException;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class GetIsExist {
+public class PostFlipdex {
 
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
     OkHttpClient client = new OkHttpClient();
 
-    String run(String url) throws IOException {
+    String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
         Request request = new Request.Builder()
                 .url(url)
+                .post(body)
                 .build();
-
         try (Response response = client.newCall(request).execute()) {
             return response.body().string();
         }
     }
+
+    String bowlingJson() {
+        return "{'title':'HIGH_SCORE', 'id': 'tilek', 'body': 'tiwlk'}";
+    }
+
     public static void main(String[] args) throws IOException {
-        GetIsExist example = new GetIsExist();
-        String response = example.run("https://raw.github.com/square/okhttp/master/README.md");
+        PostFlipdex example = new PostFlipdex();
+        String json = example.bowlingJson();
+        String response = example.post("https://flipdex.ru/ajax/setsite", json);
         System.out.println(response);
+
     }
 }
