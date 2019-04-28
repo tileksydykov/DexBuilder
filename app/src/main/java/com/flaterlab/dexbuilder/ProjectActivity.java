b2d091a950d556e20b5b228bc4d027248fb34bce
SyimykDaniyarov <mobforkill@gmail.com>
2019-04-28 12:01:17 +0600

    project add inflaters

--- app/src/main/java/com/flaterlab/dexbuilder/ProjectActivity.java
+++ app/src/main/java/com/flaterlab/dexbuilder/ProjectActivity.java
@@ -8,34 +8,38 @@ import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class ProjectActivity extends AppCompatActivity {
     LayoutInflater inflater;
-    RelativeLayout mMainContainer;
-
+    ViewGroup mMainContainer;
+    View v;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-
+                    mMainContainer.removeAllViews();
+                    v = inflater.inflate(R.layout.navbar_edit_layout, mMainContainer);
                     return true;
                 case R.id.navigation_dashboard:
-                    View v = inflater.inflate(R.layout.navbar_edit_layout, mMainContainer);
-                    v.animate();
-
-
+                    mMainContainer.removeAllViews();
+                    v = inflater.inflate(R.layout.body_edit_layout, mMainContainer);
                     return true;
                 case R.id.navigation_footer:
-
+                    mMainContainer.removeAllViews();
+                    v = inflater.inflate(R.layout.content_edit_layout, mMainContainer);
                     return true;
                 case R.id.navigation_publish:
-
+                    mMainContainer.removeAllViews();
+                    v = inflater.inflate(R.layout.save_project_layout, mMainContainer);
                     return true;
             }
             return false;
@@ -44,12 +48,14 @@ public class ProjectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        mMainContainer = findViewById(R.id.project_activity_main_frame);
+        setContentView(R.layout.activity_project);
+
         inflater = (LayoutInflater) getBaseContext()
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        setContentView(R.layout.activity_project);
+        LinearLayout view = findViewById(R.id.project_activity_main_frame);
+        mMainContainer = (ViewGroup) view;
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
