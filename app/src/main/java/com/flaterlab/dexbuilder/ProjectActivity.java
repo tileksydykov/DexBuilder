94b231bee9baad9614b7c4f9287a2f2e154fd754
TilekSydykov <tilek.sydykov@iaau.edu.kg>
2019-04-29 16:37:38 +0600

    v0.0.9

--- app/src/main/java/com/flaterlab/dexbuilder/ProjectActivity.java
+++ app/src/main/java/com/flaterlab/dexbuilder/ProjectActivity.java
@@ -27,15 +27,22 @@ import io.paperdb.Paper;
 public class ProjectActivity extends AppCompatActivity {
     final String TAG  = "Project activity";
 
+    final int EDIT_PROJECT_NAVBAR = 1;
+    final int EDIT_PROJECT_BODY = 2;
+    final int EDIT_PROJECT_SETTING = 3;
+    final int EDIT_PROJECT_SAVE = 4;
+
     LayoutInflater inflater;
     ViewGroup mMainContainer;
     View v;
-    Button mSaveProjectButton;
     Button mPublishProjectButton;
     EditText mEditSiteName;
     EditText mEditJumbotronTitle;
     EditText mEditJumbotronText;
     HashMap<String, String> project;
+    String projectName;
+
+    int currentProjectEditView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -46,17 +53,20 @@ public class ProjectActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
 
+                    saveAllChanges();
+
                     mMainContainer.removeAllViews();
 
                     v = inflater.inflate(R.layout.navbar_edit_layout, mMainContainer);
 
-                    mEditSiteName =  v.findViewById(R.id.edit_site_name);
-                    mEditJumbotronTitle =  v.findViewById(R.id.edit_jumbotron_title);
-                    mEditJumbotronText =  v.findViewById(R.id.edit_jumbotron_text);
-                    onNavbarEditCreate();
+                    onNavbarEditCreate(v);
                     return true;
 
                 case R.id.navigation_dashboard:
+
+                    saveAllChanges();
+
+                    currentProjectEditView = EDIT_PROJECT_BODY;
 
                     mMainContainer.removeAllViews();
 
@@ -66,6 +76,10 @@ public class ProjectActivity extends AppCompatActivity {
 
                 case R.id.navigation_footer:
 
+                    saveAllChanges();
+
+                    currentProjectEditView = EDIT_PROJECT_SETTING;
+
                     mMainContainer.removeAllViews();
 
                     v = inflater.inflate(R.layout.settings_edit_layout, mMainContainer);
@@ -74,11 +88,14 @@ public class ProjectActivity extends AppCompatActivity {
 
                 case R.id.navigation_publish:
 
+                    saveAllChanges();
+
+                    currentProjectEditView = EDIT_PROJECT_SAVE;
+
                     mMainContainer.removeAllViews();
 
                     v = inflater.inflate(R.layout.save_project_layout, mMainContainer);
 
-                    mSaveProjectButton = v.findViewById(R.id.button_save);
                     mPublishProjectButton = v.findViewById(R.id.button_publish);
 
                     setListenersOnSaveProjectView();
@@ -99,13 +116,13 @@ public class ProjectActivity extends AppCompatActivity {
             return;
         }
 
-        String projectName = extras.getString("projectName");
+        projectName = extras.getString("projectName");
 
         project = Paper.book(DBConfig.PROJECT_NODE)
                 .read( projectName,
                         new HashMap<String, String>());
 
-        onNavbarEditCreate();
+
         // get data via the key
         if (projectName != null) {
             // do something with the data
@@ -118,28 +135,76 @@ public class ProjectActivity extends AppCompatActivity {
         v = inflater.inflate(R.layout.navbar_edit_layout, mMainContainer);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        onNavbarEditCreate(v);
     }
 
-    protected void saveAllChanges(View v){
+    protected void saveAllChanges(){
+        switch (currentProjectEditView){
+            case EDIT_PROJECT_NAVBAR:
+
+                project.put(DBConfig.JUMBOTRON_TEXT,
+                        mEditJumbotronText.getText().toString());
+                project.put(DBConfig.JUMBOTRON_TITLE,
+                        mEditJumbotronTitle.getText().toString());
+
+                project.put(DBConfig.NAVBAR_TITLE,
+                        mEditSiteName.getText().toString());
+
+            case EDIT_PROJECT_BODY:
+
+            case EDIT_PROJECT_SAVE:
+
+            case EDIT_PROJECT_SETTING:
+
+        }
 
     }
     protected void setListenersOnSaveProjectView(){
-        mSaveProjectButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "saved", Toast.LENGTH_LONG).show();
-            }
-        });
         mPublishProjectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Toast.makeText(getApplicationContext(), "published", Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    protected void onNavbarEditCreate(){
+    protected void onNavbarEditCreate(View v){
+        currentProjectEditView = EDIT_PROJECT_NAVBAR;
+        mEditSiteName =  v.findViewById(R.id.edit_site_name);
+        mEditJumbotronTitle =  v.findViewById(R.id.edit_jumbotron_title);
+        mEditJumbotronText =  v.findViewById(R.id.edit_jumbotron_text);
+
         Log.d(TAG, "onNavbarEditCreate: " + project.toString());
         mEditJumbotronText.setText(project.get(DBConfig.JUMBOTRON_TEXT));
+        mEditJumbotronTitle.setText(project.get(DBConfig.JUMBOTRON_TITLE));
+        mEditSiteName.setText(project.get(DBConfig.NAVBAR_TITLE));
+    }
+
+    protected void saveAllProjectChanges(){
+        Paper.book(DBConfig.PROJECT_NODE).write(projectName, project);
+        Toast.makeText(getApplicationContext(), "saved", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        saveAllChanges();
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onStop() {
+        saveAllChanges();
+        saveAllProjectChanges();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        project = Paper.book(DBConfig.PROJECT_NODE)
+                .read( projectName,
+                        new HashMap<String, String>());
+        super.onStart();
     }
 }
